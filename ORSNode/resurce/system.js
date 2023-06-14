c22e110f3adde273c5f38573aa632a4bbfad117f
kamal0025 <kamalrathod0025@gmail.com>
2023-06-14 12:17:09 +0530

    Amendments of credentials

--- ORSNode/resurce/system.js
+++ ORSNode/resurce/system.js
@@ -3,7 +3,7 @@ var system = {
     service: "gmail",
     user: "krathod.rathod@gmail.com",
     pass: "Krathod@23.",
-    user1: "kalpana"
+    user1: "kalpana",
   },
   database: {
     host: "localhost",
@@ -12,10 +12,6 @@ var system = {
     database: "project_5",
     port: "3306",
     extraport: "4584",
-    masterpass: "4875",
-    andinmpass: "7874",
-    livpass: "1547",
-    databasecredential: "787498",
   },
 };
 
