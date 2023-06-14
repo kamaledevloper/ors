e23fea1d248d753800dde7af339862fa9ae293f9
kamal0025 <kamalrathod0025@gmail.com>
2023-06-14 09:20:05 +0530

    Updated with new credential

--- ORSNode/resurce/system.js
+++ ORSNode/resurce/system.js
@@ -1,21 +1,21 @@
 var system = {
-    mail: {
-        service: 'gmail',
-        user: "krathod.rathod@gmail.com",
-        pass: "Krathod@23."
-    },
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'project_5',
-        port: '3306'
-        extra post : "4584",
-        masterpass : "4875",
-        andinmpass : "7874",
-        
-    }
-}
-
+  mail: {
+    service: "gmail",
+    user: "krathod.rathod@gmail.com",
+    pass: "Krathod@23.",
+  },
+  database: {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "project_5",
+    port: "3306",
+    extraport: "4584",
+    masterpass: "4875",
+    andinmpass: "7874",
+    livpass: "1547",
+    databasecredential: "787498",
+  },
+};
 
 module.exports = system;
